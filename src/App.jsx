ac544ac7caeb1fc0bcabb7d6541cc039306d3e40
emanuelfleitas <emanuelfleitas2020@gmail.com>
2021-06-18 12:31:54 -0300

    cam

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import Hola from "./components/Hola";
+
+function App() {
+  return <div className="App">
+    <h1>Titulo de nuestro proyecto</h1>
+    <Hola></Hola>
+    </div>;
+  
+}
+
+export default App;
